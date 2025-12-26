bccd269d43d690296a7f76f104d3283d56c44279
Inc44 <incognita44@outlook.com>
2025-12-26 05:00:54 +0100

    piece image repaint

--- src/main/java/im/bpu/hexachess/ui/PieceImageLoader.java
+++ src/main/java/im/bpu/hexachess/ui/PieceImageLoader.java
@@ -11,25 +11,25 @@ class PieceImageLoader {
 	private static final Map<String, Image> images = new HashMap<>();
 	private static boolean loaded = false;
 	private static void loadImage(String key, int[] loadedCount, Runnable onload) {
-		try {
-			Image img = new Image(BASE_URL + key + ".png", true);
-			img.progressProperty().addListener((observable, oldValue, newValue) -> {
-				if (newValue.doubleValue() == 1.0) {
-					loadedCount[0]++;
-					if (loadedCount[0] == TOTAL_IMAGES) {
-						loaded = true;
-						if (onload != null)
-							onload.run();
-					}
+		Image img = new Image(BASE_URL + key + ".png", true);
+		img.progressProperty().addListener((observable, oldValue, newValue) -> {
+			if (!img.isError() && newValue.doubleValue() == 1.0) {
+				loadedCount[0]++;
+				if (loadedCount[0] == TOTAL_IMAGES) {
+					loaded = true;
+					if (onload != null)
+						onload.run();
 				}
-			});
-			images.put(key, img);
-		} catch (Exception exception) {
-			System.err.println("Exception: " + exception.getMessage());
-		}
+			}
+		});
+		images.put(key, img);
 	}
 	static void loadImages(Runnable onload) {
-		loaded = false;
+		if (loaded) {
+			if (onload != null)
+				onload.run();
+			return;
+		}
 		int[] loadedCount = {0};
 		for (String c : new String[] {"w", "b"})
 			for (String t : new String[] {"p", "r", "n", "b", "q", "k"})
